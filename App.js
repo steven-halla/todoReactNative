02f972c0d1ec55cd875d1710e573f54da3e313c4
steven-halla <69405488+steven-halla@users.noreply.github.com>
2021-07-20 16:03:12 -0700

    "third"

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, TouchableOpacity, Text, View, KeyboardAvoidingView, TextInput } from 'react-native';
 import Task from './components/Task';
 
 export default function App() {
@@ -9,10 +9,20 @@ export default function App() {
         <Text style={styles.sectionTitle}>Today's tasks</Text>
 
         <View style={styles.items}>
-          <Task text={'Task 1'}/>
-          <Task text={'Task 2'}/>
+          <Task text={'Task 1'} />
+          <Task text={'Task 2'} />
         </View>
       </View>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        style={styles.writeTaskWrapper}>
+        <TextInput style={styles.input} placeholder={'Write a task'} />
+        <TouchableOpacity >
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}> + </Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </View>
   );
 }
@@ -33,4 +43,32 @@ const styles = StyleSheet.create({
   items: {
     marginTop: 30,
   },
+  writeTaskWrapper: {
+    position: 'absolute',
+    bottom: 60,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+    width: 250,
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+  },
+  addText: {},
 });
