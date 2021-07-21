d875f5dab07a19f384ac9eaed4cd814f607c9d43
steven-halla <69405488+steven-halla@users.noreply.github.com>
2021-07-20 17:45:07 -0700

    "finish"

--- App.js
+++ App.js
@@ -1,25 +1,50 @@
-import React from 'react';
-import { StyleSheet, TouchableOpacity, Text, View, KeyboardAvoidingView, TextInput } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, TouchableOpacity, Text, View, KeyboardAvoidingView, TextInput, Keyboard } from "react-native";
 import Task from './components/Task';
 
 export default function App() {
+  const [task, setTask] = useState();
+  const [taskItems, setTaskItems] = useState([]);
+
+  const handleAddTask = () => {
+    Keyboard.dismiss();
+    setTaskItems([...taskItems, task]);
+    setTask(null);
+  };
+
+  const completeTask = (index) => {
+    let itemsCopy = [...taskItems];
+    itemsCopy.splice(index, 1);
+    setTaskItems(itemsCopy);
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.tasksWrapper}>
-        <Text style={styles.sectionTitle}>Today's tasks</Text>
+        <Text style={styles.sectionTitle}>Today's tass</Text>
 
         <View style={styles.items}>
-          <Task text={'Task 1'} />
-          <Task text={'Task 2'} />
+          {taskItems.map((item, index) => {
+            return (
+              <TouchableOpacity key={index} onPress={() => completeTask(index)}>
+                <Task text={item} />
+              </TouchableOpacity>
+            );
+          })}
         </View>
       </View>
       <KeyboardAvoidingView
         behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
         style={styles.writeTaskWrapper}>
-        <TextInput style={styles.input} placeholder={'Write a task'} />
-        <TouchableOpacity >
+        <TextInput
+          style={styles.input}
+          placeholder={'Write a task'}
+          value={task}
+          onChangeText={text => setTask(text)}
+        />
+        <TouchableOpacity onPress={() => handleAddTask()}>
           <View style={styles.addWrapper}>
-            <Text style={styles.addText}> + </Text>
+            <Text style={styles.addText}>+</Text>
           </View>
         </TouchableOpacity>
       </KeyboardAvoidingView>
